1fb8242f6da4d08d27da104978f831da33fd98c6
couteauD <598731667@qq.com>
2019-09-03 22:16:42 +0800

    用资源名称获取字符串并设置到TextView

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -2,13 +2,22 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.text_view_hello_world);
+        Context context=this.getApplicationContext();
+        String resname="hello_android";
+        int helloandroidID=context.getResources().getIdentifier(resname,"string",context.getPackageName());
+        textView.setText(this.getString(helloandroidID));
+
     }
 }
